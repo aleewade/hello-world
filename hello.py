f425d0b2499af0f1fe93dfad3a127e1b6e34f731
A Lee Wade <aleewade@users.noreply.github.com>
2026-07-06 11:42:59 -0400

    Add hello world program

--- hello.py
+++ hello.py
@@ -1,20 +1,6 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+def main():
+    print("Hello, world!")
 
-PORT = 8080
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        body = b"<!DOCTYPE html><html><head><meta charset='utf-8'><title>Hola</title></head><body><h1>\xc2\xa1Hola, Mundo!</h1></body></html>"
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html; charset=utf-8")
-        self.send_header("Content-Length", len(body))
-        self.end_headers()
-        self.wfile.write(body)
-
-    def log_message(self, format, *args):
-        pass
 
 if __name__ == "__main__":
-    server = HTTPServer(("", PORT), Handler)
-    print(f"Listening on http://localhost:{PORT}")
-    server.serve_forever()
+    main()
